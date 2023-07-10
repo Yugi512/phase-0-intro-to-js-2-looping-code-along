47f410aedceb9fc31863827088b7edf4fae8468a
Kelvin Gil <gilkelvin2@gmail.com>
2023-07-10 14:33:25 -0400

    done

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+function writeCards(nameS,name){
+    for (let i = 0;i < nameS.length;i++){
+        const samething = Thank you, ${nameS[i]}, for the wonderful ${name} gift!;
+        names.push(samething);
+    }
+   return names;
+}
+writeCards(names);
+
 function countDown(i){
     let x = i;
     while( x >= 0){
